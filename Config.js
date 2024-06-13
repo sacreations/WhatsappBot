16b3f11cfdfc12516685fbe39e55f9f3578ea426
sacreations <supunadithya@hotmail.com>
2024-06-13 11:51:02 +0530

    refactor: Update Config.js with default values for bot info and options

--- Config.js
+++ Config.js
@@ -3,15 +3,15 @@ require('dotenv').config({ path: './config.env' });
 const config = {
 
     // Bot Info
-    bot_number:process.env.BOT_NUMBER,
-    bot_name:process.env.BOT_NAME,
-    bot_owner:process.env.BOT_OWNER,
+    bot_number:process.env.BOT_NUMBER || "",
+    bot_name:process.env.BOT_NAME || "WhatsappBot",
+    bot_owner:process.env.BOT_OWNER || "SA_Creations",
 
     // Options
-    db_update:process.env.DB_UPDATE,
-    auto_reply:process.env.AUTO_REPLY,
-    start_message:process.env.DISABLE_START_MESSAGE,
-    PREFIX:process.env.PREFIX,
+    db_update:process.env.DB_UPDATE || false,
+    auto_reply:process.env.AUTO_REPLY || false,
+    start_message:process.env.DISABLE_START_MESSAGE || false,
+    PREFIX:process.env.PREFIX || ".",
 
 
     // Databases
@@ -25,7 +25,7 @@ const config = {
 
     // API 
     apiKey: process.env.API_KEY,
-    api_Url: process.env.API_URL,
+    api_Url: process.env.API_URL || "https://api.sacreations.me",
 };
 
 module.exports = config;
