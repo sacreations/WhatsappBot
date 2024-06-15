d55624786ee1c55530a13d214aad2e7d8e4ad30b
sacreations <supunadithya@hotmail.com>
2024-06-15 08:38:29 +0530

    `refactor: Update update_bot.sh to ensure script is run on a system with apt package manager`

--- update_bot.sh
+++ update_bot.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# Switch to the dev_test branch
+# Ensure the script is run on a system with the apt package manager
+if ! [ -x "$(command -v apt)" ]; then
+    echo -e "\e[31mThis script is intended for use on Linux systems with the apt package manager.\e[0m"
+    exit 1
+fi
+
+echo -e "\e[36mStarting the update process for the dev_test branch...\e[0m"
+
+
+# Ensure we are on the dev_test branch
 git checkout dev_test
 
 # Pull the latest changes from the dev_test branch
